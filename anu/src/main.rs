95bf3689bba0a8d18b268a9d813ae27d2499d7f4
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-01-24 13:59:27 +0100

    feat(anu): menu selection input

--- anu/src/main.rs
+++ anu/src/main.rs
@@ -1,12 +1,27 @@
 use std::io;
 
+const INVALID_MENU_CHOICE_MESSAGE: &'static str = "You must enter a valid menu option";
+
 fn main() {
-    let err_message = String::from("You must enter a valid number");
-    let guess = input_u8(err_message);
-    println!("{guess}");
+    run_main_menu();
 }
 
-fn input_u8(err_message: String) -> u8 {
+fn run_main_menu() {
+    const CHOICES: [&'static str; 3] = [
+        "Quit",
+        "Games",
+        "Shopping list"
+    ];
+
+    match bound(INVALID_MENU_CHOICE_MESSAGE, 0, CHOICES.len() as u8) {
+        0 => println!("Option 0"),
+        1 => println!("Option 1"),
+        2 => println!("Option 2"),
+        _ => unreachable!()
+    }
+}
+
+fn input_u8(err_message: &str) -> u8 {
     let mut input = String::new();
 
     io::stdin()
@@ -20,4 +35,15 @@ fn input_u8(err_message: String) -> u8 {
             input_u8(err_message)
         }
     }
+}
+
+fn bound(err_message: &str, lower_bound: u8, upper_bound: u8) -> u8 {
+    let n = input_u8(err_message);
+    if n >= lower_bound && n < upper_bound {
+        n
+    } else {
+        println!("{}", err_message);
+        bound(err_message, lower_bound, upper_bound)
+    }
+    
 }
