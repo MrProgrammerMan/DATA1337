9f386f1ea82d1ee566b0b26adaf94aee39dde299
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-01-24 13:19:32 +0100

    feat(anu): u8 input

--- anu/src/main.rs
+++ anu/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let err_message = String::from("You must enter a valid number");
+    let guess = input_u8(err_message);
+    println!("{guess}");
+}
+
+fn input_u8(err_message: String) -> u8 {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    match input.trim().parse::<u8>() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("{}", err_message);
+            input_u8(err_message)
+        }
+    }
 }
